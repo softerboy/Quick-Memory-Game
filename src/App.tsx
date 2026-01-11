1b4c132ad50be96c70c1738a35dee4c339a14034
nightowl <fn.umarov@gmail.com>
2026-01-11 19:09:43 +0500

    Integrate Redux Toolkit and Counter feature
    - Added Redux Toolkit configuration in `src/store/index.ts` for state management.
    - Created `counterSlice` in `src/store/features/counter/counterSlice.ts` with basic `increment` and `decrement` actions.
    - Added custom hooks `useAppDispatch` and `useAppSelector` in `src/hooks/store.ts`.
    - Refactored `App.tsx` to connect UI with Redux store for managing counter state.
    - Updated `main.tsx` to wrap the app with `Provider` for Redux store access.
    - Updated `package.json` and `pnpm-lock.yaml` to include `@reduxjs/toolkit` and `react-redux` dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import type { FC } from 'react'
+
 import { Button } from '@/components/ui/button'
 
+import { useAppDispatch, useAppSelector } from '@/hooks/store.ts'
+import { decrement, increment } from '@/store/features/counter/counterSlice.ts'
+
 const App: FC = () => {
+  const count = useAppSelector(state => state.counter.count)
+  const dispatch = useAppDispatch()
+
   return (
     <div className="max-w-7xl mx-auto p-8 text-center">
       <h1 className="text-4xl font-bold mb-4">Memory Game</h1>
       <p className="text-lg mb-6">Welcome to the Memory Game!</p>
       <div className="flex justify-center gap-4">
-        <Button variant="default">Start Game</Button>
-        <Button variant="outline">How to Play</Button>
+        <Button onClick={() => dispatch(increment())} variant="default">
+          Start Game
+        </Button>
+        <Button onClick={() => dispatch(decrement())} variant="outline">
+          How to Play
+        </Button>
+
+        <h1>{count}</h1>
       </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+
+import { store } from '@/store'
 
 import './index.css'
 import './App.css'
@@ -7,6 +10,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
